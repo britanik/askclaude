a9f331a657fb20fddf8e0890a5cbc5459fd6ec1d
britanik <britannic@bk.ru>
2024-07-07 08:48:11 +0400

    Claude refactor

--- app/controllers/navigation.ts
+++ app/controllers/navigation.ts
@@ -1,15 +1,12 @@
 import TelegramBot from "node-telegram-bot-api"
-
 import { IUser } from "../interfaces/users"
 import Dict from "../helpers/dict"
-
 import * as userController from "./users"
 import { IMenuButton } from "../interfaces/menu-button"
 import { isMenuClicked } from "./menu"
 import { sendMessage } from "../templates/sendMessage"
 import { tmplRegisterLang } from "../templates/tmplRegisterLanguage"
 import { handleAssistantReply, handleUserReply, startAssistant } from "./assistants"
-import { promptsDict } from "../helpers/prompts"
 import { tmplAdmin } from "../templates/tmplAdmin"
 
 export interface INavigationParams {
@@ -22,123 +19,96 @@ export interface INavigationParams {
 }
 
 export default class Navigation {
-  
-  user:IUser
-  data
-  msg
-  dict: Dict
-  bot:TelegramBot
-  mediaGroups
-  rest
+  private user: IUser
+  private data: any
+  private msg: any
+  private dict: Dict
+  private bot: TelegramBot
+  private mediaGroups: number[]
+  private rest: {
+    user: IUser
+    msg: any
+    dict: Dict
+    data: any
+    bot: TelegramBot
+  }
 
-  constructor( params:INavigationParams ){
-    this.user        = params.user
-    this.bot         = params.bot
-    this.msg         = params.msg
+  constructor(params: INavigationParams) {
+    this.user = params.user
+    this.bot = params.bot
+    this.msg = params.msg
     this.mediaGroups = params.mediaGroups
-    this.dict        = params.dict
+    this.dict = params.dict
 
-    if( params.callbackQuery ){
-      this.data = JSON.parse( params.callbackQuery.data )
+    if (params.callbackQuery) {
+      this.data = JSON.parse(params.callbackQuery.data)
     }
 
-    this.rest = { 
-      user: this.user, 
-      msg: this.msg, 
+    this.rest = {
+      user: this.user,
+      msg: this.msg,
       dict: this.dict,
-      data: this.data, 
-      bot: this.bot 
+      data: this.data,
+      bot: this.bot,
     }
   }
 
-  async build(){
-  // далее это либо
-  // Menu: Мой профиль, Новая встреча, Настройки, Мои встречи
-  // Ответ на просьбу ввести текст или отправить фото (step)
-  // Комманда /new /my_profile /my_settings /add_bio /add_photo /active
+  async build() {
+    const method = this.determineMethod()
+    const actionType = this.determineActionType(method)
 
-    // if( !userController.hasBioAndPhoto(this.user) && !isRegistration((this.data)? this.data.a :) ){
-    //   await this.regAskPhoto().action()
-    //   return
-    // }
-    let method:string
-    let whatIsIt:string
-    let step = userController.getStep( this.user )
-    
-    // if this is callbackQuery (inline button)
-    if( this.data && this[this.data.a] ){
-      method = this.data.a
-      whatIsIt = 'callback'
-    }
-
-    // if this is message
-    if( this.msg ){
-      let menuItem:IMenuButton | false = isMenuClicked( this.msg )
-      if( menuItem ){
-        
-        // Menu or command
-        whatIsIt = 'action'
-        method = menuItem.method
-        
-        // Reset this.user messages, step and data properties
-        await this.resetUser()
-
-      } else {
-
-        // Input or photo (if step) or just simple text (if no step)
-        whatIsIt = 'callback'
-        if( step ){
-          method = step
-        }
-      }
-    }
-
-    if( !this[method] && this.msg ){
-      // await addLog({ method: 'text', ...this.rest })
+    if (!method && this.msg) {
       await this.notFound().action()
       return
     }
 
-    // if user account deleted
-    // if( this.user.deleted ){
-    //   if( method == 'start' && this.data.v ){
-    //     await this.start().callback()
-    //   } else {
-    //     await this.start().action()
-    //   }
-    // }
-    
-    // if( !userController.hasBioAndPhoto(this.user) && !this[method]().registration ){
-    //   await addLog({ method: 'redirectProfileNotFullуBefore', ...this.rest })
-    //   this.user = await userController.addData(this.user, 'redirectTo', method)
-    //   await this.settingsRedirect().action()
-    //   return
-    // }
-
-    console.log('execute', method, whatIsIt)
-    // execute
-    await this[method]()[whatIsIt]()
+    console.log('execute', method, actionType)
+    await this[method]()[actionType]()
   }
 
-  async resetUser(){
-    console.log('reset user')
-    
-    // reset messages
-    for (const key in this.user.messages) {
-      if (Object.prototype.hasOwnProperty.call(this.user.messages, key)) {
-        this.user.messages[key] = null;
+  private determineMethod(): string | null {
+    if (this.data && this[this.data.a]) {
+      return this.data.a
+    }
+
+    if (this.msg) {
+      const menuItem: IMenuButton | false = isMenuClicked(this.msg)
+      if (menuItem) {
+        return menuItem.method
+      }
+
+      const step = userController.getStep(this.user)
+      if (step) {
+        return step
       }
     }
 
-    // reset step and data
+    return null
+  }
+
+  private determineActionType(method: string): 'action' | 'callback' {
+    if (this.data && this[method]) {
+      return 'callback'
+    }
+
+    if (this.msg) {
+      const menuItem: IMenuButton | false = isMenuClicked(this.msg)
+      return menuItem ? 'action' : 'callback'
+    }
+
+    return 'action'
+  }
+
+  private async resetUser() {
+    console.log('reset user')
+    this.user.messages = {}
     this.user.step = null
     this.user.data = null
-    
-    // save
     this.user = await this.user.save()
   }
 
-  start(){
+  // Navigation methods
+  start() {
     return {
       action: async () => {
         await this.language().action()
@@ -147,7 +117,7 @@ export default class Navigation {
     }
   }
 
-  new(){
+  new() {
     return {
       action: async () => {
         await this.assistant().action()
@@ -156,52 +126,51 @@ export default class Navigation {
     }
   }
 
-  language(){
+  language() {
     return {
       action: async () => {
-        await tmplRegisterLang( this.user, this.dict, this.bot )
+        await tmplRegisterLang(this.user, this.dict, this.bot)
       },
       callback: async () => {
-        this.user = await userController.updatePref( this.user, 'lang', this.data.v )
-        this.dict.setLang( this.data.v )
-
+        this.user = await userController.updatePref(this.user, 'lang', this.data.v)
+        this.dict.setLang(this.data.v)
         await this.assistant().action()
       },
     }
   }
 
-  assistant(){
+  assistant() {
     return {
       action: async () => {
         console.log('Assistant()')
         this.user = await userController.addStep(this.user, 'assistant')
         const firstMessage = this.dict.getString('ASSISTANT_START')
-        let thread = await startAssistant( this.user, firstMessage )
+        let thread = await startAssistant(this.user, firstMessage)
         await handleAssistantReply(thread, this.bot, this.dict)
       },
       callback: async () => {
         try {
-          const userReply = (this.data?.v) ? this.dict.getString(this.data.v) : this.msg.text
+          const userReply = this.data?.v ? this.dict.getString(this.data.v) : this.msg.text
           let threadWithUserMessage = await handleUserReply(this.user, userReply, this.bot)
           await handleAssistantReply(threadWithUserMessage, this.bot, this.dict)
-        } catch(e){
-          console.log('Failed to settingsAboutAssistant callback', e)
+        } catch (e) {
+          console.error('Failed to handle assistant callback', e)
           await sendMessage({ text: this.dict.getString('ASSISTANT_ERROR'), user: this.user, bot: this.bot })
         }
       },
     }
   }
 
-  admin(){
+  admin() {
     return {
       action: async () => {
-        await tmplAdmin( this.user, this.bot )
+        await tmplAdmin(this.user, this.bot)
       },
       callback: async () => {},
     }
   }
 
-  notFound(){
+  notFound() {
     return {
       action: async () => {
         await sendMessage({ text: this.dict.getString('NOT_FOUND'), user: this.user, bot: this.bot })
@@ -211,12 +180,4 @@ export default class Navigation {
       },
     }
   }
-
-  example(){
-    return {
-      action: async () => {},
-      callback: async () => {},
-    }
-  }
-
 }
