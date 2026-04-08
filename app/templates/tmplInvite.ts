8e709b9daf24f23a0675c84ed8f55b4e0ba883ae
Petr Loginov <britannic@bk.ru>
2026-04-08 08:56:06 +0400

    Stories - adding open button to tmplInvite

--- app/templates/tmplInvite.ts
+++ app/templates/tmplInvite.ts
@@ -7,10 +7,15 @@ import { generateInviteCode } from "../controllers/invites";
 export async function tmplInvite(user: IUser, bot: TelegramBot, dict: Dict) {
   // Get or generate an invite code for this user
   const inviteCode = await generateInviteCode(user);
-  
+
   // Create the invitation message with the user's unique code
   const text = `Попробуй AskClaude AI в Telegram. Общайся текстом, голосовыми сообщениям, фотографиями. Ищи в интернете. Генерируй картинки. Ссылка: https://t.me/${process.env.CLIENT_BOT_NAME}?start=${inviteCode}`;
-  
+
+  const appUrl = `https://askclaude.ru/app?code=${inviteCode}`;
+  const buttons = [[
+    { text: '📲 Пригласить в сторис', web_app: { url: appUrl } } as any
+  ]];
+
   await sendMessage({ text: 'Перешли следующее сообщение:', user, bot });
-  await sendMessage({ text, user, bot });
+  await sendMessage({ text, user, bot, buttons });
 }
